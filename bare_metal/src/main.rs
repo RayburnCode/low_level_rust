c7e3232477af3ba499747081012647360550dea3
RayburnCode <rayburncode@gmail.com>
2025-12-13 11:41:30 -0800

    bump

--- bare_metal/src/main.rs
+++ bare_metal/src/main.rs
@@ -1,21 +1,19 @@
 #![no_std]
 #![no_main]
 
-
 use core::panic::PanicInfo;
 
 mod vga_buffer;
 
-/// This function is called on panic.
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
     loop {}
 }
 
-
-#[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
-
+/// This function is called on panic.
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
--- embedded_rust_book/Notes.md
+++
@@ -1,7 +0,0 @@
-<!-- @format -->
-
-# Sources
-
-1. Rust Embedded MB2 Discovery Book <https://docs.rust-embedded.org/discovery-mb2/>
-2. Awesome Embedded Rust <https://github.com/rust-embedded/awesome-embedded-rust>
-3. Rust Raspberry Pi OS Tutorial <https://github.com/rust-embedded/rust-raspberrypi-OS-tutorials>

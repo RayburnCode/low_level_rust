533c3cbadb319eae332f1a5eea00c16e00ddfc77
RayburnCode <rayburncode@gmail.com>
2025-12-15 21:04:38 -0800

    bump

--- bare_metal/04_ Testing.md
+++ bare_metal/04_ Testing.md
@@ -4,3 +4,15 @@
 
 - Rust has a built-in test framework that can run unit tests without the need to set anything up
 - Rust’s test framework uses the built-in test library, which depends on the standard library. Which isnt' possible with using #[no_std]
+
+## Notes
+
+- Memory Mapped I/O vs Port Mapped I/O
+- port-mapped I/O uses a separate I/O bus for communication.
+- CPU instructions called in and out, which take a port number and a data byte (there are also variations of these commands that allow sending a u16 or u32)
+
+### UART
+
+- Universal asynchronous receiver-transmitter
+- Data dent in one by one bits, from the least to most significant
+- Common signal levels are RS-232, RS-485, and raw TTL(Transistor-Transistor Logic)
--- bare_metal/src/main.rs
+++ bare_metal/src/main.rs
@@ -1,34 +1,46 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(bare_metal::test_runner)]
+#![test_runner(blog_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use bare_metal::println;
-use core::panic::PanicInfo;
+use blog_os::serial_println;
+use core::{panic::PanicInfo, ptr, slice};
 
-#[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
+#[no_mangle]
+pub extern "C" fn _start(boot_info: &'static mut bootloader::boot_info::BootInfo) -> ! {
     #[cfg(test)]
     test_main();
 
-    loop {}
+    let mut framebuffer = {
+        let ptr = boot_info.framebuffer.start_addr as *mut u8;
+        let slice = unsafe { slice::from_raw_parts_mut(ptr, boot_info.framebuffer.len) };
+        volatile::Volatile::new(slice)
+    };
+
+    //serial_println!("Hello World{}", "!");
+
+    for i in 0..boot_info.framebuffer.len {
+        framebuffer.index_mut(i).write(0x99);
+    }
+
+    loop {
+        x86_64::instructions::hlt();
+    }
 }
 
 /// This function is called on panic.
 #[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
+    serial_println!("{}", info);
     loop {}
 }
 
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    bare_metal::test_panic_handler(info)
+    blog_os::test_panic_handler(info)
 }
 
 #[test_case]
